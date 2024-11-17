99d978c6279fe3b6703681af02efc7399f1fe038
Sreinumder <rojgosai02@gmail.com>
2024-11-17 22:54:44 +0545

    more language..

--- .config/nvim/lua/nvchad/configs/treesitter.lua
+++ .config/nvim/lua/nvchad/configs/treesitter.lua
@@ -5,7 +5,11 @@ end)
 
 return {
   ensure_installed = { "lua", "luadoc", "printf", "vim", "vimdoc" },
-
+  matchup = {
+    enable = true,              -- mandatory, false will disable the whole extension
+    disable = { "c", "ruby" },  -- optional, list of language that will be disabled
+    -- [options]
+  },
   highlight = {
     enable = true,
     use_languagetree = true,
