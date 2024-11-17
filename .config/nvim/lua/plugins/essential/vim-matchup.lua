12f766ed2541d6529b94c584852a69a06bdb0585
Sreinumder <rojgosai02@gmail.com>
2024-11-17 23:00:53 +0545

    no lazy

--- .config/nvim/lua/plugins/essential/vim-matchup.lua
+++ .config/nvim/lua/plugins/essential/vim-matchup.lua
@@ -1,10 +1,10 @@
 return {
 	"andymass/vim-matchup",
 	dependencies = {"nvim-treesitter/nvim-treesitter"},
-  event = "UIEnter",
-	keys = {{"%"}},
-	-- opts = {},
-	config = function()
-		vim.g.matchup_matchparen_offscreen = { method = "popup" }
-	end,
+  lazy = false,
+  -- event = "UIEnter",
+	-- keys = {{"%"}},
+  config = function()
+    vim.g.matchup_matchparen_offscreen = { method = "popup"}
+  end,
 }
