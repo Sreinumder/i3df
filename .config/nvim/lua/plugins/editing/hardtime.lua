530e9e1e0ba5c51984eee664c726a3121e7b0cbc
Sreinumder <rojgosai02@gmail.com>
2024-11-17 22:55:51 +0545

    dont like hardtimes

--- .config/nvim/lua/plugins/editing/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "m4xshen/hardtime.nvim",
-  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {},
-  events = {"VeryLazy"},
-  keys = {
-    { "<leader>tth", ":Hardtime toggle<CR>", { desc = "Toggle hardtime" } },
-  }
-}
