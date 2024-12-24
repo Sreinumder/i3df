de0dc323c64143715c7ba8278b26b7d465a4aa47
Sreinumder <rojgosai02@gmail.com>
2024-12-25 01:17:34 +0545

    fix(wezterm): change to right

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -55,7 +55,7 @@ config.keys = {
 			local panes = tab:panes_with_info()
 			if #panes == 1 then
 				pane:split({
-					direction = "Down",
+					direction = "Right",
 					size = 0.4,
 				})
 			elseif not panes[1].is_zoomed then
