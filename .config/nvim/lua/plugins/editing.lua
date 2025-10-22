a425d3f2837ff8c0294829528345456e5be9a093
Sreinumder <rojgosai02@gmail.com>
2025-10-22 19:00:19 +0545

    flow

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -14,138 +14,138 @@ return {
     })
   end,
 },
-{ "monaqa/dial.nvim",
-event = "InsertEnter",
-keys = {
-  {"<leader>i"},
-  {"<leader>u"},
-  {"g<leader>i"},
-  {"g<leader>u"},
-},
-enabled = true,
-config = function()
-  local augend = require("dial.augend")
-  local dial_config = require("dial.config")
-
-  local function concat_tables(table1, table2)
-    if table2 == nil then
-      return table1
-    end
-    local result = table1 or {}
-    for _, value in ipairs(table2) do
-      result = concat_tables(result, value)
-    end
-    return result
-  end
-
-  local function AGN(Elements, Word, Cyclic)
-    Word = true and Word
-    Cyclic = true and Cyclic
-    if Elements == nil then
-      return nil
-    end
-    return augend.constant.new({ elements = Elements, word = Word, cyclic = Cyclic })
-  end
-
-  local augend_base = {
-    augend.integer.alias.decimal_int, -- decimal number (-1, 0, 1, 2, 3, ...)
-    augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
-    augend.hexcolor.new({ case = "lower" }),
-    augend.hexcolor.new({ case = "upper" }),
-  }
-  dial_config.augends:register_group({
-    default = augend_base,
-    number = {
-      augend.integer.alias.decimal_int, -- decimal number (-1, 0, 1, 2, 3, ...)
-      augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
-    },
-    logical = {
-      augend.constant.alias.bool, -- boolean value (true <-> false)
-      AGN({ "True", "False" }),
-      AGN({ "and", "or" }),
-      AGN({ "&&", "||" }, false),
-      AGN({ "==", "!=" }, false),
-      AGN({ "<", ">=" }, false),
-      AGN({ ">", "<=" }, false),
-      AGN({ "+=", "-=" }, false),
-      AGN({ "*=", "/=" }, false),
-      AGN({ "++", "--" }, false),
-    },
-    word = {
-      AGN({ "yes", "no" }),
-      AGN({ "left", "right" }),
-      AGN({ "up", "down" }),
-      AGN({ "mini.cycle", "when" }),
-      AGN({ "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth" }),
-      AGN({ "First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth" }),
-    },
-    alpha = { augend.constant.alias.alpha, augend.constant.alias.Alpha }, -- a b c  A B C
-    quote = { augend.paren.alias.quote }, -- 'rose'
-    parenthesis = { augend.paren.alias.brackets },
-    date = {
-      augend.date.alias["%Y/%m/%d"], -- date (2024/04/23, etc.)
-      augend.date.alias["%d/%m/%Y"], -- date (24/04/2024, etc.)
-      augend.date.alias["%Y-%m-%d"], -- date (2024-04-23, etc.)
-      augend.date.alias["%d-%m-%Y"], -- date (24-04-2024, etc.)
-      -- augend.date.alias["%Y-%m-%d"], -- date (2024-04-23, etc.)
-      -- augend.date.alias["%d-%m-%Y"], -- date (24-04-2024, etc.)
-      augend.date.alias["%Y年%-m月%-d日(%ja)"], -- date (2024年-04月-20日, etc.)
-      augend.date.alias["%Y年%-m月%-d日"],
-      AGN({ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }),
-      AGN({"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",}),
-      augend.constant.alias.ja_weekday,
-      augend.constant.alias.ja_weekday_full, -- 月 金曜日
-      AGN({ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }),
-      AGN({ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" }),
-    },
-    time = {
-      augend.date.alias["%H:%M:%S"],
-      augend.date.alias["%H:%M"],
-    },
-    notes = {
-      AGN({ "- [ ]", "- [x]" }, false),
-      AGN({ "=====", "-----" }, false),
-      AGN({ "*", "**", "***", "****", "*****", "******", "*******" }, false),
-      AGN({ "# ", "## ", "### ", "#### ", "##### ", "###### " }, false),
-      -- AGN({"┘","┐","c", "└", "┌"}, false),
-      -- AGN({"e","─","│"}, false),
-      -- AGN({"◄", "h","►", "▼","▲"}, false),
-    },
-  })
-  dial_config.augends:on_filetype({
-    typescript = concat_tables(augend_base, { augend.constant.new({ elements = { "let", "const" } }) }),
-    javascript = concat_tables(augend_base, { augend.constant.new({ elements = { "let", "const" } }) }),
-    haskell = concat_tables(augend_base, {
-      augend.constant.new({ elements = { "/=", "==" }, word = false, cyclic = true }),
-    }),
-  })
-  local mode = { "n", "v" }
-  local arga = { "increment", "decrement" }
-  local argb = { "normal", "gnormal", "visual", "gvisual" }
-  local group =
-  { "default", "number", "date", "time", "word", "alpha", "logical", "parenthesis", "quote", "notes" }
-  local keymaps = {
-    { "<C-a>", "<C-x>", "g<C-a>", "g<C-x>" },
-    -- {"<leader>ii",  "<leader>uu",  "g<leader>ii",  "g<leader>uu"},-- "default
-    { "<leader>iu", "<leader>uu", "g<leader>iu", "g<leader>uu" }, -- "number"
-    { "<leader>id", "<leader>ud", "g<leader>id", "g<leader>ud" }, -- "date",
-    { "<leader>it", "<leader>ut", "g<leader>it", "g<leader>ut" }, -- "time",
-    { "<leader>iw", "<leader>uw", "g<leader>iw", "g<leader>uw" }, -- "word",
-    { "<leader>ia", "<leader>ua", "g<leader>ia", "g<leader>ua" }, -- "alpha
-    { "<leader>il", "<leader>ul", "g<leader>il", "g<leader>ul" }, -- "logical
-    { "<leader>ip", "<leader>up", "g<leader>ip", "g<leader>up" }, -- "parenthesis
-    { "<leader>iq", "<leader>uq", "g<leader>iq", "g<leader>uq" }, -- "quote",
-    { "<leader>is", "<leader>us", "g<leader>is", "g<leader>us" }, -- "notes",
-  }
-  for j = 1, #group do
-    for i = 0, 7 do
-      vim.keymap.set(mode[math.floor(i / 4) + 1], keymaps[j][i % 4 + 1], function()
-        require("dial.map").manipulate(arga[i % 2 + 1], argb[math.floor(i / 2) + 1], group[j])
-      end, {desc = group[j]})
-    end
-  end
-end,
-  },
+-- { "monaqa/dial.nvim",
+-- event = "InsertEnter",
+-- keys = {
+--   {"<leader>i"},
+--   {"<leader>u"},
+--   {"g<leader>i"},
+--   {"g<leader>u"},
+-- },
+-- enabled = true,
+-- config = function()
+--   local augend = require("dial.augend")
+--   local dial_config = require("dial.config")
+--
+--   local function concat_tables(table1, table2)
+--     if table2 == nil then
+--       return table1
+--     end
+--     local result = table1 or {}
+--     for _, value in ipairs(table2) do
+--       result = concat_tables(result, value)
+--     end
+--     return result
+--   end
+--
+--   local function AGN(Elements, Word, Cyclic)
+--     Word = true and Word
+--     Cyclic = true and Cyclic
+--     if Elements == nil then
+--       return nil
+--     end
+--     return augend.constant.new({ elements = Elements, word = Word, cyclic = Cyclic })
+--   end
+--
+--   local augend_base = {
+--     augend.integer.alias.decimal_int, -- decimal number (-1, 0, 1, 2, 3, ...)
+--     augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+--     augend.hexcolor.new({ case = "lower" }),
+--     augend.hexcolor.new({ case = "upper" }),
+--   }
+--   dial_config.augends:register_group({
+--     default = augend_base,
+--     number = {
+--       augend.integer.alias.decimal_int, -- decimal number (-1, 0, 1, 2, 3, ...)
+--       augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+--     },
+--     logical = {
+--       augend.constant.alias.bool, -- boolean value (true <-> false)
+--       AGN({ "True", "False" }),
+--       AGN({ "and", "or" }),
+--       AGN({ "&&", "||" }, false),
+--       AGN({ "==", "!=" }, false),
+--       AGN({ "<", ">=" }, false),
+--       AGN({ ">", "<=" }, false),
+--       AGN({ "+=", "-=" }, false),
+--       AGN({ "*=", "/=" }, false),
+--       AGN({ "++", "--" }, false),
+--     },
+--     word = {
+--       AGN({ "yes", "no" }),
+--       AGN({ "left", "right" }),
+--       AGN({ "up", "down" }),
+--       AGN({ "mini.cycle", "when" }),
+--       AGN({ "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth" }),
+--       AGN({ "First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth" }),
+--     },
+--     alpha = { augend.constant.alias.alpha, augend.constant.alias.Alpha }, -- a b c  A B C
+--     quote = { augend.paren.alias.quote }, -- 'rose'
+--     parenthesis = { augend.paren.alias.brackets },
+--     date = {
+--       augend.date.alias["%Y/%m/%d"], -- date (2024/04/23, etc.)
+--       augend.date.alias["%d/%m/%Y"], -- date (24/04/2024, etc.)
+--       augend.date.alias["%Y-%m-%d"], -- date (2024-04-23, etc.)
+--       augend.date.alias["%d-%m-%Y"], -- date (24-04-2024, etc.)
+--       -- augend.date.alias["%Y-%m-%d"], -- date (2024-04-23, etc.)
+--       -- augend.date.alias["%d-%m-%Y"], -- date (24-04-2024, etc.)
+--       augend.date.alias["%Y年%-m月%-d日(%ja)"], -- date (2024年-04月-20日, etc.)
+--       augend.date.alias["%Y年%-m月%-d日"],
+--       AGN({ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" }),
+--       AGN({"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December",}),
+--       augend.constant.alias.ja_weekday,
+--       augend.constant.alias.ja_weekday_full, -- 月 金曜日
+--       AGN({ "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" }),
+--       AGN({ "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" }),
+--     },
+--     time = {
+--       augend.date.alias["%H:%M:%S"],
+--       augend.date.alias["%H:%M"],
+--     },
+--     notes = {
+--       AGN({ "- [ ]", "- [x]" }, false),
+--       AGN({ "=====", "-----" }, false),
+--       AGN({ "*", "**", "***", "****", "*****", "******", "*******" }, false),
+--       AGN({ "# ", "## ", "### ", "#### ", "##### ", "###### " }, false),
+--       -- AGN({"┘","┐","c", "└", "┌"}, false),
+--       -- AGN({"e","─","│"}, false),
+--       -- AGN({"◄", "h","►", "▼","▲"}, false),
+--     },
+--   })
+--   dial_config.augends:on_filetype({
+--     typescript = concat_tables(augend_base, { augend.constant.new({ elements = { "let", "const" } }) }),
+--     javascript = concat_tables(augend_base, { augend.constant.new({ elements = { "let", "const" } }) }),
+--     haskell = concat_tables(augend_base, {
+--       augend.constant.new({ elements = { "/=", "==" }, word = false, cyclic = true }),
+--     }),
+--   })
+--   local mode = { "n", "v" }
+--   local arga = { "increment", "decrement" }
+--   local argb = { "normal", "gnormal", "visual", "gvisual" }
+--   local group =
+--   { "default", "number", "date", "time", "word", "alpha", "logical", "parenthesis", "quote", "notes" }
+--   local keymaps = {
+--     { "<C-a>", "<C-x>", "g<C-a>", "g<C-x>" },
+--     -- {"<leader>ii",  "<leader>uu",  "g<leader>ii",  "g<leader>uu"},-- "default
+--     { "<leader>iu", "<leader>uu", "g<leader>iu", "g<leader>uu" }, -- "number"
+--     { "<leader>id", "<leader>ud", "g<leader>id", "g<leader>ud" }, -- "date",
+--     { "<leader>it", "<leader>ut", "g<leader>it", "g<leader>ut" }, -- "time",
+--     { "<leader>iw", "<leader>uw", "g<leader>iw", "g<leader>uw" }, -- "word",
+--     { "<leader>ia", "<leader>ua", "g<leader>ia", "g<leader>ua" }, -- "alpha
+--     { "<leader>il", "<leader>ul", "g<leader>il", "g<leader>ul" }, -- "logical
+--     { "<leader>ip", "<leader>up", "g<leader>ip", "g<leader>up" }, -- "parenthesis
+--     { "<leader>iq", "<leader>uq", "g<leader>iq", "g<leader>uq" }, -- "quote",
+--     { "<leader>is", "<leader>us", "g<leader>is", "g<leader>us" }, -- "notes",
+--   }
+--   for j = 1, #group do
+--     for i = 0, 7 do
+--       vim.keymap.set(mode[math.floor(i / 4) + 1], keymaps[j][i % 4 + 1], function()
+--         require("dial.map").manipulate(arga[i % 2 + 1], argb[math.floor(i / 2) + 1], group[j])
+--       end, {desc = group[j]})
+--     end
+--   end
+-- end,
+--   },
   {
     "folke/flash.nvim",
     opts = {
@@ -416,5 +416,37 @@ end,
       { modes = { "n", "x" }, "<leader>dtt", "<cmd>lua MiniTrailspace.trim_last_lines()<CR>" ,desc = "delete_traillines" },
       { modes = { "n", "x" }, "<leader>dte", "<cmd>lua MiniTrailspace.trim()<CR>" ,desc = "delete_trailspace" },
     }
+  },
+  {
+    'jbyuki/venn.nvim',
+    -- opts = {},
+    lazy = false,
+    config = function()
+      function _G.Toggle_venn()
+        local venn_enabled = vim.inspect(vim.b.venn_enabled)
+        if venn_enabled == "nil" then
+          vim.b.venn_enabled = true
+          vim.cmd[[setlocal ve=all]]
+          -- draw a line on HJKL keystokes
+          vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox<CR>", {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox<CR>", {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox<CR>", {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox<CR>", {noremap = true})
+          -- draw a box by pressing "f" with visual selection
+          vim.api.nvim_buf_set_keymap(0, "v", "f", ":VBox<CR>", {noremap = true})
+          vim.api.nvim_buf_set_keymap(0, "v", "F", ":VBoxD<CR>", {noremap = true})
+        else
+          vim.cmd[[setlocal ve=]]
+          vim.api.nvim_buf_del_keymap(0, "n", "J")
+          vim.api.nvim_buf_del_keymap(0, "n", "K")
+          vim.api.nvim_buf_del_keymap(0, "n", "L")
+          vim.api.nvim_buf_del_keymap(0, "n", "H")
+          vim.api.nvim_buf_del_keymap(0, "v", "f")
+          vim.b.venn_enabled = nil
+        end
+      end
+
+      vim.api.nvim_set_keymap('n', ';v', ":lua Toggle_venn()<CR>", { noremap = true})
+    end,
   }
 }
