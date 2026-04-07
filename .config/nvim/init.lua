fcebc114c617a8cbe886c0271dc135b59d0958df
Sreinumder <rojgosai02@gmail.com>
2026-04-07 14:41:32 +0545

    non-transparent when out of focus

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,7 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ";"
 
-vim.cmd("autocmd BufEnter * set formatoptions-=cro")
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.uv.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--single-branch",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath,
+    })
     })
 end
 vim.opt.rtp:prepend(lazypath)
