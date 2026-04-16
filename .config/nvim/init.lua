0733bb783ca0ce1600e37ea26bfceeffdccaf567
Sreinumder <rojgosai02@gmail.com>
2026-04-16 22:29:54 +0545

    venn.nvim setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -367,6 +367,39 @@ end, {
 })
 -- }}}
 
+-- venn.nvim :VBoxD {{{
+vim.pack.add({'https://github.com/jbyuki/venn.nvim'})
+require('venn')
+-- venn.nvim: enable or disable keymappings
+function _G.Toggle_venn(mode)
+    local venn_enabled = vim.inspect(vim.b.venn_enabled)
+    if venn_enabled == "nil" then
+        vim.b.venn_enabled = true
+        vim.cmd[[setlocal ve=all]]
+        -- draw a line on HJKL keystokes
+        vim.api.nvim_buf_set_keymap(0, "n", "J", "<C-v>j:VBox" .. mode .."<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "K", "<C-v>k:VBox" .. mode .."<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "L", "<C-v>l:VBox" .. mode .."<CR>", {noremap = true})
+        vim.api.nvim_buf_set_keymap(0, "n", "H", "<C-v>h:VBox" .. mode .."<CR>", {noremap = true})
+        -- draw a box by pressing "f" with visual selection
+        vim.api.nvim_buf_set_keymap(0, "v", "f", "<cmd>VBox" .. mode .."<CR>", {noremap = true})
+    else
+        vim.cmd[[setlocal ve=]]
+        vim.api.nvim_buf_del_keymap(0, "n", "J")
+        vim.api.nvim_buf_del_keymap(0, "n", "K")
+        vim.api.nvim_buf_del_keymap(0, "n", "L")
+        vim.api.nvim_buf_del_keymap(0, "n", "H")
+        vim.api.nvim_buf_del_keymap(0, "v", "f")
+        vim.b.venn_enabled = nil
+    end
+end
+-- toggle keymappings for venn using <leader>v
+vim.api.nvim_set_keymap('n', '<leader>vv', "<cmd>lua Toggle_venn('')<CR>", { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>vd', "<cmd>lua Toggle_venn('V')<CR>", { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>vh', "<cmd>lua Toggle_venn('D')<CR>", { noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>vo', "<cmd>lua Toggle_venn('O')<CR>", { noremap = true})
+-- }}}
+
 -- FileType settings{{{
 vim.filetype.add({
     extension = {
